8d42f8528bc0585d3413574c0dea2d741e60cc29
TubatMark <72871470+TubatMark@users.noreply.github.com>
2023-06-19 08:28:40 -0700

    update login

--- login.php
+++ login.php
@@ -5,23 +5,23 @@
 	$password = mysqli_real_escape_string($con, $_POST['password']);
 	$user_result = mysqli_query($con, "SELECT * FROM users WHERE email='$email'");
 
-	echo "welcome maderpaker";
+	#echo "welcome maderpaker";
 
 
-	// if ($user_result && mysqli_num_rows($user_result) > 0) {
-	// 	$user = mysqli_fetch_assoc($user_result);
-	// 	$user_type = $user['user_type'];
+	if ($user_result && mysqli_num_rows($user_result) > 0) {
+		$user = mysqli_fetch_assoc($user_result);
+		$user_type = $user['user_type'];
 
-	// 	if ($user_type == 'admin') {
-	// 		header("Location: admin/admin_page.php");
-	// 		exit();
-	// 	} elseif ($user_type == 'cashier') {
-	// 		header("Location: employees/cashier_page.php");
-	// 		exit();
-	// 	} else {
-	// 		echo "No record found. Please register first.";
-	// 	}
-	// } else {
-	// 	echo "No record found. Please register first.";
-	// }
+		if ($user_type == 'admin') {
+			echo "paker";
+			exit();
+		} elseif ($user_type == 'cashier') {
+			echo "mader";
+			exit();
+		} else {
+			echo "No record found. Please register first.";
+		}
+	} else {
+		echo "No record found. Please register first.";
+	}
 ?>
