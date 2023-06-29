cb83b4cc5621aae9784901872d5e1688ecfe400e
TubatMark <72871470+TubatMark@users.noreply.github.com>
2023-06-29 19:52:49 +0800

    .

--- users/employees/employee_page.php
+++ users/employees/employee_page.php
@@ -1,34 +1,36 @@
-<header>    
+<header>
     <?php include '../employees/includes/cashier_links.php' ?>
 </header>
+
 <body>
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">AetherIO</a>
-  </div>
-</nav>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">AetherIO</a>
+        </div>
+    </nav>
 
-<div class="container-fluid pt-3">
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="card">
-            <div class="row">
-                <div class="col-lg-8">
-                   basic
-                </div>
-                <div class="col-lg-4">
-                    <img src="/print.png" class="img-fluid" alt="meh">
-                    <i class="bi bi-heart"></i>
-                    <i class="bi bi-heart text-danger fs-4 mr-2"></i>
+    <div class="container-fluid pt-3">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="row">
+                        <div class="col-lg-8">
+                            basic
+                        </div>
+                        <div class="col-lg-4">
+                            <img src="cashier_styles/pictures/print.png" class="img-fluid" alt="meh">
+                            <i class="bi bi-heart"></i>
+                            <i class="bi bi-heart text-danger fs-4 mr-2"></i>
 
 
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-      </div>
     </div>
-  </div>
 
     <?php include "../employees/includes/cashier_scripts.php" ?>
 </body>
+
 </html>
