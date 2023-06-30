0d7b330e90b688e5b0e9c7da033be5b7865808bb
BeltranJayson <nostalgic221@gmail.com>
2023-06-30 20:46:38 +0800

    meh

--- users/employees/employee_page.php
+++ users/employees/employee_page.php
@@ -1,28 +1,29 @@
 <header>
     <?php include '../employees/includes/cashier_links.php' ?>
+
 </header>
 
 <body>
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">AetherIO</a>
-        </div>
+    <div class="container-fluid">
+        <span class="navbar-text">AetherIO</span>
+    </div>
     </nav>
-
     <div class="container-fluid pt-3">
         <div class="row">
             <div class="col-lg-12">
                 <div class="card">
                     <div class="row">
                         <div class="col-lg-8">
-                            basic
+                            sample
                         </div>
                         <div class="col-lg-4">
-                            <img src="cashier_styles/pictures/print.png" class="img-fluid" alt="meh">
-                            <i class="bi bi-heart"></i>
-                            <i class="bi bi-heart text-danger fs-4 mr-2"></i>
-
-
+                            <img src="cashier_styles/pictures/print.png" class="img-fluid" alt="meh" style="width:450px">
+                            <div class="cright" >
+                                <img src="cashier_styles/pictures/search-heart.svg" width="40px" height="40px">Search<a href="Package.php"></a><br>
+                                <img src="cashier_styles/pictures/upc-scan.svg" width="40px" height="40px">Barcode<a href="Package.php"></a><br>
+                                <img src="cashier_styles/pictures/box2-heart-fill.svg" width="40px" height="40px">Package<a href="Package.php"></a><br>
+                            </div>
                         </div>
                     </div>
                 </div>
