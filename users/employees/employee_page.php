7f38cfd7b4ac082d724084e37200fe9bf50fe1f9
BeltranJayson <nostalgic221@gmail.com>
2023-07-01 00:08:28 +0800

    modal pa lng

--- users/employees/employee_page.php
+++ users/employees/employee_page.php
@@ -2,9 +2,9 @@
 
 <body>
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-    <div class="container-fluid">
-        <span class="navbar-text">AetherIO</span>
-    </div>
+        <div class="container-fluid">
+            <span class="navbar-text">AetherIO</span>
+        </div>
     </nav>
     <div class="container-fluid pt-3">
         <div class="row">
@@ -16,10 +16,91 @@
                         </div>
                         <div class="col-lg-4">
                             <img src="cashier_styles/pictures/print.png" class="img-fluid" alt="meh" style="width:450px">
-                            <div class="cright" >
-                                <img src="cashier_styles/pictures/search-heart.svg" width="40px" height="40px">Search<a href="Package.php"></a><br>
-                                <img src="cashier_styles/pictures/upc-scan.svg" width="40px" height="40px">Barcode<a href="Package.php"></a><br>
-                                <img src="cashier_styles/pictures/box2-heart-fill.svg" width="40px" height="40px">Package<a href="Package.php"></a><br>
+                            <div class="cright">
+                                <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#myModal">
+                                <img src="cashier_styles/pictures/search-heart.svg" width="40px" height="40px">Search<br>
+                                </button>
+
+                                <!-- The Modal -->
+                                <div class="modal" id="myModal">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+
+                                            <!-- Modal Header -->
+                                            <div class="modal-header">
+                                                <h4 class="modal-title">Modal Heading</h4>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                            </div>
+
+                                            <!-- Modal body -->
+                                            <div class="modal-body">
+                                                Modal body..
+                                            </div>
+
+                                            <!-- Modal footer -->
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#myModal">
+                                <img src="cashier_styles/pictures/upc-scan.svg" width="40px" height="40px">Barcode<br>
+                                </button>
+
+                                <!-- The Modal -->
+                                <div class="modal" id="myModal">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+
+                                            <!-- Modal Header -->
+                                            <div class="modal-header">
+                                                <h4 class="modal-title">Modal Heading</h4>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                            </div>
+
+                                            <!-- Modal body -->
+                                            <div class="modal-body">
+                                                Modal body..
+                                            </div>
+
+                                            <!-- Modal footer -->
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#myModal">
+                                <img src="cashier_styles/pictures/box2-heart-fill.svg" width="40px" height="40px">Package<br>
+                                </button>
+
+                                <!-- The Modal -->
+                                <div class="modal" id="myModal">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+
+                                            <!-- Modal Header -->
+                                            <div class="modal-header">
+                                                <h4 class="modal-title">Modal Heading</h4>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                            </div>
+
+                                            <!-- Modal body -->
+                                            <div class="modal-body">
+                                                Modal body..
+                                            </div>
+
+                                            <!-- Modal footer -->
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
