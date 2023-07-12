f48830966f6e3fc1bcd14c70ee2b8daa90fe7057
TubatMark <72871470+TubatMark@users.noreply.github.com>
2023-07-12 09:50:49 +0800

    admin side

---
+++ users/admin/admin_includes/home.php
@@ -0,0 +1,35 @@
+<?php include "header.php" ?>
+
+<body>
+    <div class="card-container">
+        <div class="row1">
+            <div class="card">INVOICING</div>
+            <div class="card">INVENTORY</div>
+            <div class="card">GENERATE P.O. INVENTORY</div>
+            <div class="card">INVENTORY LIST</div>
+        </div>
+        <div class="row2">
+            <div class="card">CUSTOMER INFO</div>
+            <div class="card">SUPPLIER INFO</div>
+            <div class="card">REPORTS</div>
+            <div class="card">STATEMENT OF ACCOUNT</div>
+        </div>
+        <div class="row3">
+            <div class="card">ACCOUNT MANAGEMENT</div>
+            <div class="card">ACCOUNTS RECEIVABLE</div>
+            <div class="card">ACCOUNTS PAYABLE</div>
+            <div class="card">ADDRESS BOOK</div>
+        </div>
+
+        <div class="card-info">
+            <div class="info">
+                <header>STORE INFORMATION</header>
+            </div>
+        </div>
+    </div>
+
+    
+
+</body>
+
+</html>
--- users/admin/admin_includes/links.php
+++ users/admin/admin_includes/links.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="../admin/styles/css/sidebar.css">
     <link rel="stylesheet" href="../admin/styles/css/admin_dashboard.css">
+    <link rel="stylesheet" href="../styles/css/home.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link flex href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
     
