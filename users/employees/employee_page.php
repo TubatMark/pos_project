474327cd8d6be15c6cc9681faf5febb7d9454e58
BeltranJayson <73050520+BeltranJayson@users.noreply.github.com>
2023-07-18 21:22:43 +0800

    unrefined-table

--- users/employees/employee_page.php
+++ users/employees/employee_page.php
@@ -20,7 +20,8 @@
                                 <div class="col-lg-9">
                                     <div class="top">
                                         <h4 id="h4">French Fries</h4>
-                                        <button type="button" class="btn btn-danger" id="btn-delete">Delete Item</button>
+                                        <button type="button" class="btn btn-danger" id="btn-delete">Delete
+                                            Item</button>
 
                                     </div>
                                     <div class="mid">
@@ -39,49 +40,111 @@
                                     <h6>Total PHP</h6>
                                     <h4 id="h41">₱1250.25</h4>
                                 </div> -->
+
+                            </div>
+                            <div class="container table-container" style="height: 300px; overflow-y: auto;">
+                                <div class="container mt-3">
+                                    <h2>Table Head Colors</h2>
+                                    <p>You can use any of the contextual classes to only add a color to the table
+                                        header:</p>
+                                    <table class="table">
+                                        <thead class="table-dark">
+                                            <tr>
+                                                <th>Firstname</th>
+                                                <th>Lastname</th>
+                                                <th>Email</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>John</td>
+                                                <td>Doe</td>
+                                                <td>john@example.com</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Mary</td>
+                                                <td>Moe</td>
+                                                <td>mary@example.com</td>
+                                            </tr>
+                                            <tr>
+                                                <td>July</td>
+                                                <td>Dooley</td>
+                                                <td>july@example.com</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                    <table class="table">
+                                        <thead class="table-success">
+                                            <tr>
+                                                <th>Firstname</th>
+                                                <th>Lastname</th>
+                                                <th>Email</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>John</td>
+                                                <td>Doe</td>
+                                                <td>john@example.com</td>
+                                            </tr>
+                                            <tr>
+                                                <td>Mary</td>
+                                                <td>Moe</td>
+                                                <td>mary@example.com</td>
+                                            </tr>
+                                            <tr>
+                                                <td>July</td>
+                                                <td>Dooley</td>
+                                                <td>july@example.com</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
 
                         </div>
+
                         <div class="col-lg-4">
                             <img src="cashier_styles/pictures/print.png" class="leftimg" alt="meh">
                             <div class="cright">
                                 <?php include '../employees/cashier_modals/cashier_right_modal.php' ?>
                             </div>
                         </div>
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
     <script>
-  // Get the quantity input and buttons
-  const quantityInput = document.querySelector('.quantity');
-  const minusBtn = document.querySelector('.minus-btn');
-  const plusBtn = document.querySelector('.plus-btn');
+        // Get the quantity input and buttons
+        const quantityInput = document.querySelector('.quantity');
+        const minusBtn = document.querySelector('.minus-btn');
+        const plusBtn = document.querySelector('.plus-btn');
 
-  // Add event listeners to buttons
-  minusBtn.addEventListener('click', () => {
-    decreaseQuantity();
-  });
+        // Add event listeners to buttons
+        minusBtn.addEventListener('click', () => {
+            decreaseQuantity();
+        });
 
-  plusBtn.addEventListener('click', () => {
-    increaseQuantity();
-  });
+        plusBtn.addEventListener('click', () => {
+            increaseQuantity();
+        });
 
-  // Function to decrease quantity
-  function decreaseQuantity() {
-    const currentQuantity = parseInt(quantityInput.value);
-    if (currentQuantity > 1) {
-      quantityInput.value = currentQuantity - 1;
-    }
-  }
+        // Function to decrease quantity
+        function decreaseQuantity() {
+            const currentQuantity = parseInt(quantityInput.value);
+            if (currentQuantity > 1) {
+                quantityInput.value = currentQuantity - 1;
+            }
+        }
 
-  // Function to increase quantity
-  function increaseQuantity() {
-    const currentQuantity = parseInt(quantityInput.value);
-    quantityInput.value = currentQuantity + 1;
-  }
-</script>
+        // Function to increase quantity
+        function increaseQuantity() {
+            const currentQuantity = parseInt(quantityInput.value);
+            quantityInput.value = currentQuantity + 1;
+        }
+    </script>
     <?php include "../employees/includes/cashier_scripts.php" ?>
 </body>
 
