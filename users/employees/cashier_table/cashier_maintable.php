cd2f179d3aa09319af5d26e44a4385200b25b3c9
BeltranJayson <73050520+BeltranJayson@users.noreply.github.com>
2023-07-24 01:48:58 +0800

    arranged table

---
+++ users/employees/cashier_table/cashier_maintable.php
@@ -0,0 +1,49 @@
+<div class="container mt-3">
+    <h2>List of Goods</h2>
+    <table class="table">
+        <thead class="table-dark">
+            <tr>
+                <th>Name</th>
+                <th>Quantity</th>
+                <th>Price Php</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Coke 1.5L</td>
+                <td>2</td>
+                <td>65</td>
+            </tr>
+            <tr>
+                <td>Sprite 1.5L</td>
+                <td>2</td>
+                <td>67</td>
+            </tr>
+            <tr>
+                <td>Royal Tru Orange 1.5L</td>
+                <td>3</td>
+                <td>60</td>
+            </tr>
+            <tr>
+                <td>C2 Green Tea</td>
+                <td>5</td>
+                <td>20</td>
+            </tr>
+            <tr>
+                <td>Nutri Star</td>
+                <td>8</td>
+                <td>18</td>
+            </tr>
+            <tr>
+                <td>Star Corned Beef</td>
+                <td>3</td>
+                <td>35</td>
+            </tr>
+            <tr>
+                <td>San Marino Corned Tuna</td>
+                <td>5</td>
+                <td>22</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
--- users/employees/employee_page.php
+++ users/employees/employee_page.php
@@ -43,63 +43,7 @@
 
                             </div>
                             <div class="container table-container" style="height: 300px; overflow-y: auto;">
-                                <div class="container mt-3">
-                                    <h2>Table Head Colors</h2>
-                                    <p>You can use any of the contextual classes to only add a color to the table
-                                        header:</p>
-                                    <table class="table">
-                                        <thead class="table-dark">
-                                            <tr>
-                                                <th>Firstname</th>
-                                                <th>Lastname</th>
-                                                <th>Email</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>John</td>
-                                                <td>Doe</td>
-                                                <td>john@example.com</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Mary</td>
-                                                <td>Moe</td>
-                                                <td>mary@example.com</td>
-                                            </tr>
-                                            <tr>
-                                                <td>July</td>
-                                                <td>Dooley</td>
-                                                <td>july@example.com</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                    <table class="table">
-                                        <thead class="table-success">
-                                            <tr>
-                                                <th>Firstname</th>
-                                                <th>Lastname</th>
-                                                <th>Email</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <tr>
-                                                <td>John</td>
-                                                <td>Doe</td>
-                                                <td>john@example.com</td>
-                                            </tr>
-                                            <tr>
-                                                <td>Mary</td>
-                                                <td>Moe</td>
-                                                <td>mary@example.com</td>
-                                            </tr>
-                                            <tr>
-                                                <td>July</td>
-                                                <td>Dooley</td>
-                                                <td>july@example.com</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
+                                <?php include "../employees/cashier_table/cashier_maintable.php" ?>
                             </div>
 
                         </div>
